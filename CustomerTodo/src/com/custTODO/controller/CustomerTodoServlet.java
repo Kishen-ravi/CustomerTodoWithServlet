42ec561be6c34fd489b1a778c939dccda751cd70
kishen <kishen.chandran@a-cti.com>
2017-11-02 18:22:14 +0530

    Change in package name

--- CustomerTodo/src/com/custTODO/controller/CustomerTodoServlet.java
+++ CustomerTodo/src/com/custTODO/controller/CustomerTodoServlet.java
@@ -1,4 +1,4 @@
-package com.custTODO;
+package com.custTODO.controller;
 
 import java.io.IOException;
 import java.util.List;
--- CustomerTodo/src/com/custTODO/controller/LoadListServlet.java
+++ CustomerTodo/src/com/custTODO/controller/LoadListServlet.java
@@ -1,4 +1,4 @@
-package com.custTODO;
+package com.custTODO.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,25 +12,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.custTODO.JDO.CustomerJDO;
+import com.custTODO.JDO.TodoListJDO;
 import com.custTODO.JDO.PMF;
 import com.google.gson.Gson;
 
-public class LoadCustomerServlet extends HttpServlet {
+public class LoadListServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		HttpSession session = req.getSession();
 		String custEmail = session.getAttribute("sessionname").toString();
+//		String custKey = req.getParameter("data");
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		PrintWriter out = res.getWriter();
 		String output = "";
-		Query q = pm.newQuery(CustomerJDO.class);
+		Query q = pm.newQuery(TodoListJDO.class);
 		q.setFilter("adminEmail =='" + custEmail + "'");
 
-		List<CustomerJDO> result = (List<CustomerJDO>) q.execute();
+		List<TodoListJDO> result = (List<TodoListJDO>) q.execute();
 
-		System.out.println("no of cusotomer :" + result.size());
+		System.out.println("no of Items :" + result.size());
 		Gson gson = new Gson();
 		output = gson.toJson(result);
 		out.write(output);
